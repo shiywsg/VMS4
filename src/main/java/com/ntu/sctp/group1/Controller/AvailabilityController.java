bd496f562716a4596f6ab7eb82941cd9bceeb5d1
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-03-18 10:31:29 +0800

    commit

--- src/main/java/com/ntu/sctp/group1/Controller/AvailabilityController.java
+++ src/main/java/com/ntu/sctp/group1/Controller/AvailabilityController.java
@@ -1,6 +1,5 @@
 package com.ntu.sctp.group1.Controller;
 
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +9,6 @@ public class AvailabilityController {
 
     @PostMapping("/{volunterrId}")
     public ResponseEntity setAvailability(@PathVariable Integer volunteerId, @RequestParam String date) {
-        
+        return null;
     }
 }
