da785ae469bc93e1a146f965f565fdd782ea3348
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-05-02 15:35:58 +0800

    prepare for secret keys

---
+++ src/main/java/com/ntu/sctp/group1/DataTransferObject/FirebaseCredentials.java
@@ -0,0 +1,23 @@
+package com.ntu.sctp.group1.DataTransferObject;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class FirebaseCredentials {
+    private String type;
+    private String project_id;
+    private String private_key_id;
+    private String private_key;
+    private String client_email;
+    private String client_id;
+    private String auth_uri;
+    private String token_uri;
+    private String auth_provider_x509_cert_url;
+    private String client_x509_cert_url;
+}
--- src/main/java/com/ntu/sctp/group1/FirebaseConfiguration.java
+++ src/main/java/com/ntu/sctp/group1/FirebaseConfiguration.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ntu.sctp.group1.DataTransferObject.FirebaseCredentials;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,10 @@ public class FirebaseConfiguration {
 
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
+
+        FirebaseCredentials firebaseCredentials = new FirebaseCredentials();
+
+
        var options = FirebaseOptions.builder()
                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
                .build();
